91617a5bfc68c1a25aebcf6e9394334c78dca2fe
engin üstün <enginustun@outlook.com>
2018-02-04 03:20:11 +0300

    Add 'set global settings' function

--- dev/main.js
+++ dev/main.js
@@ -1,7 +1,18 @@
 import RestArtClient from './modules/rest-client';
 import RestArtBaseModel from './modules/base-model';
+import settings from './modules/settings';
+import helper from './common/helper';
 
 module.exports = {
   RestArtClient,
   RestArtBaseModel,
+  SetGlobalSettings(_settings) {
+    if (helper.isObject(_settings)) {
+      const settingsKeys = Object.keys(_settings);
+      for (let i = 0; i < settingsKeys.length; i += 1) {
+        const settingKey = settingsKeys[i];
+        settings[settingKey] = _settings[settingKey];
+      }
+    }
+  },
 };
