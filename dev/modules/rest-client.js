464d275a6ab4aeb3cbf9ec03cb16ca3d4ffb3acb
engin üstün <enginustun@outlook.com>
2018-02-04 03:12:28 +0300

    Implement REST consumer module

--- dev/modules/rest-client.js
+++ dev/modules/rest-client.js
@@ -1,13 +1,76 @@
-// import settings from './settings';
+import settings from './settings';
+import XHR from './xhr';
+import helper from '../common/helper';
 
 class RestArtClient {
-  // constructor() { }
+  constructor(_settings) {
+    this.settings = {
+      currentApiPath: 'default',
+    };
+    if (helper.isObject(_settings)) {
+      this.settings.endpoint = _settings.endpoint || settings.endpoint;
+      this.settings.currentApiPath = _settings.currentApiPath || settings.defaultApiPath;
+    }
+  }
 
-  // get() { }
-  // post() { }
-  // put() { }
-  // patch() { }
-  // delete() { }
+  static addApiPath(name, value) {
+    settings.apiPaths[name] = value;
+  }
+
+  static setDefaultHeaders(request) {
+    if (request instanceof XHR) {
+      request.setHeader('Accept', settings.headers.accept);
+      request.setHeader('Content-Type', settings.headers.contentType);
+    }
+  }
+
+  get(service) {
+    const request = new XHR();
+    RestArtClient.setDefaultHeaders(request);
+    request.method = 'GET';
+    request.url = this.settings.endpoint +
+      settings.apiPaths[this.settings.currentApiPath || settings.defaultApiPath] + service;
+    return request;
+  }
+
+  post(service, data) {
+    const request = new XHR();
+    RestArtClient.setDefaultHeaders(request);
+    request.method = 'POST';
+    request.url = this.settings.endpoint +
+      settings.apiPaths[this.settings.currentApiPath || settings.defaultApiPath] + service;
+    request.data = data;
+    return request;
+  }
+
+  put(service, data) {
+    const request = new XHR();
+    RestArtClient.setDefaultHeaders(request);
+    request.method = 'PUT';
+    request.url = this.settings.endpoint +
+      settings.apiPaths[this.settings.currentApiPath || settings.defaultApiPath] + service;
+    request.data = data;
+    return request;
+  }
+
+  patch(service, data) {
+    const request = new XHR();
+    RestArtClient.setDefaultHeaders(request);
+    request.method = 'PATCH';
+    request.url = this.settings.endpoint +
+      settings.apiPaths[this.settings.currentApiPath || settings.defaultApiPath] + service;
+    request.data = data;
+    return request;
+  }
+
+  delete(service) {
+    const request = new XHR();
+    RestArtClient.setDefaultHeaders(request);
+    request.method = 'DELETE';
+    request.url = this.settings.endpoint +
+      settings.apiPaths[this.settings.currentApiPath || settings.defaultApiPath] + service;
+    return request;
+  }
 }
 
 export default RestArtClient;
