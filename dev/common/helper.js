ae5643fca1bb4a1ac5fcc65e6b61d0a4c202ad5a
engin üstün <enginustun@outlook.com>
2018-02-04 20:31:35 +0300

    Add pathJoin helper method, simplified existing functions

--- dev/common/helper.js
+++ dev/common/helper.js
@@ -1,9 +1,23 @@
 module.exports = {
-  isObject: (val) => {
-    return Object.prototype.toString.call(val) === '[object Object]';
-  },
+  isObject: val => Object.prototype.toString.call(val) === '[object Object]',
 
-  isArray: (val) => {
-    return Object.prototype.toString.call(val) === '[object Array]';
+  isArray: val => Object.prototype.toString.call(val) === '[object Array]',
+
+  pathJoin: (...paths) => {
+    let resultPath = '';
+    for (let i = 0; i < paths.length; i += 1) {
+      let pathItem = paths[i];
+      const pathItemLength = pathItem.length;
+      if (pathItemLength > 0) {
+        if (pathItem[pathItemLength - 1] === '/') {
+          pathItem = pathItem.substr(0, pathItemLength - 1);
+        }
+        if (pathItem[0] === '/') {
+          pathItem = pathItem.substr(1, pathItemLength - 1);
+        }
+      }
+      resultPath += ((resultPath.length > 0 ? '/' : '') + pathItem);
+    }
+    return resultPath;
   },
 };
